bad8bb8913d03b0475540e2223a210ab5397fefc
will <william.g.neto@gmail.com>
2019-07-12 00:26:37 -0300

    first

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <h1>Calculadora</h1>
+        <p>Calculadora em React do Will</p>
+        <Calculator />
+        <p>CHUPA JAVASCRIPT !!!</p>
+    </div>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
